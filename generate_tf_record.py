08e4b63453622755cc50dcae79c08385edd16023
Trias <triasbrata@gmail.com>
2021-12-07 21:54:18 +0800

    u: add generate tf record check file

--- generate_tf_record.py
+++ generate_tf_record.py
@@ -8,6 +8,8 @@ import argparse
 import logging
 import io
 import os
+import pathlib
+
 
 from PIL import Image
 from object_detection.utils import dataset_util
@@ -46,47 +48,49 @@ class TFRecord:
 
 
     def create_tf(self, group, path):
-        with tf.io.gfile.GFile(os.path.join(path, '{}'\
-                                        .format(group.filename)), 'rb') as fid:
-            encoded_jpg = fid.read()
-        encoded_jpg_io = io.BytesIO(encoded_jpg)
-        image = Image.open(encoded_jpg_io)
-        width, height = image.size
+          image_path = os.path.join(path, '{}'.format(group.filename));
+          if pathlib.Path(image_path).exists():
+            print('image path {} not found'.format(image_path))
+          with tf.io.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+              encoded_jpg = fid.read()
+          encoded_jpg_io = io.BytesIO(encoded_jpg)
+          image = Image.open(encoded_jpg_io)
+          width, height = image.size
 
-        filename = group.filename.encode('utf8')
-        image_format = b'jpg'
-        xmins = []
-        xmaxs = []
-        ymins = []
-        ymaxs = []
-        classes_text = []
-        classes = []
+          filename = group.filename.encode('utf8')
+          image_format = b'jpg'
+          xmins = []
+          xmaxs = []
+          ymins = []
+          ymaxs = []
+          classes_text = []
+          classes = []
 
-        for index, row in group.object.iterrows():
-            xmins.append(row['xmin'] / width)
-            xmaxs.append(row['xmax'] / width)
-            ymins.append(row['ymin'] / height)
-            ymaxs.append(row['ymax'] / height)
-            classes_text.append(row['class'].encode('utf8'))
-            classes.append(self.class_text_to_int(row['class']))
+          for index, row in group.object.iterrows():
+              xmins.append(row['xmin'] / width)
+              xmaxs.append(row['xmax'] / width)
+              ymins.append(row['ymin'] / height)
+              ymaxs.append(row['ymax'] / height)
+              classes_text.append(row['class'].encode('utf8'))
+              classes.append(self.class_text_to_int(row['class']))
 
-        tf_sample = tf.train.Example(features=tf.train.Features(feature={
-            'image/height': dataset_util.int64_feature(height),
-            'image/width': dataset_util.int64_feature(width),
-            'image/filename': dataset_util.bytes_feature(filename),
-            'image/source_id': dataset_util.bytes_feature(filename),
-            'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-            'image/format': dataset_util.bytes_feature(image_format),
-            'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-            'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-            'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-            'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-            'image/object/class/text':\
-                                dataset_util.bytes_list_feature(classes_text),
-            'image/object/class/label':\
-                                    dataset_util.int64_list_feature(classes),
-        }))
-        return tf_sample
+          tf_sample = tf.train.Example(features=tf.train.Features(feature={
+              'image/height': dataset_util.int64_feature(height),
+              'image/width': dataset_util.int64_feature(width),
+              'image/filename': dataset_util.bytes_feature(filename),
+              'image/source_id': dataset_util.bytes_feature(filename),
+              'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+              'image/format': dataset_util.bytes_feature(image_format),
+              'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+              'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+              'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+              'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+              'image/object/class/text':\
+                                  dataset_util.bytes_list_feature(classes_text),
+              'image/object/class/label':\
+                                      dataset_util.int64_list_feature(classes),
+          }))
+          return tf_sample
 
     def generate(self, output_path, image_dir, csv_input) -> None:
         writer = tf.io.TFRecordWriter(output_path)
